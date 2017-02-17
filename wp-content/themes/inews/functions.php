a9c52f4a145e05e812c57834b5b1b4f1778d1ea8
Ben Keith <ben@benlk.com>
2017-02-17 18:29:23 -0500

    Fix indentation in functions.php

--- wp-content/themes/inews/functions.php
+++ wp-content/themes/inews/functions.php
@@ -22,19 +22,19 @@ if ( ! defined( 'INN_HOSTED' ) ) {
 function inews_eloqua() { ?>
 	<!-- Eloqua -->
 	<script type="text/javascript">
-	    var _elqQ = _elqQ || [];
-	    _elqQ.push(['elqSetSiteId', '590918421']);
-	    _elqQ.push(['elqTrackPageView']);
+		var _elqQ = _elqQ || [];
+		_elqQ.push(['elqSetSiteId', '590918421']);
+		_elqQ.push(['elqTrackPageView']);
 
-	    (function () {
-	        function async_load() {
-	            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
-	            s.src = '//img.en25.com/i/elqCfg.min.js';
-	            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-	        }
-	        if (window.addEventListener) window.addEventListener('DOMContentLoaded', async_load, false);
-	        else if (window.attachEvent) window.attachEvent('onload', async_load);
-	    })();
+		(function () {
+			function async_load() {
+				var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+				s.src = '//img.en25.com/i/elqCfg.min.js';
+				var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+			}
+			if (window.addEventListener) window.addEventListener('DOMContentLoaded', async_load, false);
+			else if (window.attachEvent) window.attachEvent('onload', async_load);
+		})();
 	</script>
 <?php }
 add_action( 'wp_footer', 'inews_eloqua' );
